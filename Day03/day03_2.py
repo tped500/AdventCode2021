bee7fbb8e3645e98b137085f002ec97f07d01f5c
TPedreira <tgo.pedreira@gmail.com>
2022-01-16 14:07:25 +0100

    Day 3

---
+++ Day03/day03_2.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+file = open("input_raw.txt", "r")
+input_list = [line.rstrip() for line in file.readlines()]
+
+epsilon_rate = 0
+len_input = len(input_list)
+
+gamma_final = []
+column = 0
+
+while column < len(input_list[0]):
+    sum_of_column = 0
+    for element in input_list:
+        sum_of_column += int(element[column])
+    column += 1
+
+    if sum_of_column > len_input / 2:
+        gamma_final.append(1)
+    else:
+        gamma_final.append(0)
+
+
+num = 0
+num_epsilon = 0
+for b in gamma_final:
+    num = 2 * num + b
+    num_epsilon = 2 * num_epsilon + (0 if b == 1 else 1)
+
+power_consumption = num * num_epsilon
+print(power_consumption)
+
+
