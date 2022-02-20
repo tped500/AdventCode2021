03fb4cfe9ba02bb0e55480115a0a5f2ab426a264
TPedreira <tgo.pedreira@gmail.com>
2022-02-20 13:38:05 +0000

    Day 4

--- Day03/day03_2.py
+++ Day03/day03_2.py
@@ -5,60 +5,49 @@ input_list = [line.rstrip() for line in file.readlines()]
 epsilon_rate = 0
 len_input = len(input_list)
 
-gamma_final = []
-column = 0
+# criar lista inicial com resultado de winners
+initial_sum = 0
+initial_list_winners = []
+initial_list_losers = []
 
-while column < len(input_list[0]):
-    sum_of_column = 0
-    # Calculate the more/less common
-    for element in input_list:
-        sum_of_column += int(element[column]) # iterar aqui para ver que elementos ficam
-    column += 1
-
-    if sum_of_column > len_input / 2:
-        gamma_final.append(1)
+def calculate_winner(input):
+    counter_sum = 0
+    # for element in input:
+    #      counter_sum += int(element)
+    if sum(input) > len(input)/2:
+        return 1
     else:
-        gamma_final.append(0)
+        return 0
+
+print("Mein Spatz I love you ")
+#print(calculate_winner([0, 1, 1, 0, 1]))
+
+# Calculate number of rows
+rows_length = len(list(input_list[0:]))
+print(rows_length)
+column = 0
+new_list = []
+while column < rows_length:
+    new_list.append(list(input_list[column][0]))
+    #print(new_list)
+    #print(calculate_winner(list(input_list[column[0]])))
 
 
-winner_col = 0
-winner_list = []
-loser_list = []
-# usar uma list comprehension para tirar todos os valores que comecam com cada elemento de gamma_final
-# Setting up the initial array with winners and losers
-for prev_winner in gamma_final:
-    winner_list = [element for element in input_list if element.startswith(str(gamma_final[winner_col]))]
-    loser_list = [element for element in input_list if element.startswith(str(abs(gamma_final[winner_col])-1))]
+    #    sum_of_column = calculate_winner(input_list[int(column)])
+    column += 1
+    # for element in input_list:
+    #     sum_of_column += int(element[column]) # iterar aqui para ver que elementos ficam
+    # column += 1
+    #
+    # if sum_of_column > len_input / 2:
+    #     gamma_final.append(1)
+    # else:
+    #     gamma_final.append(0)
+    #
 
-# Check if we are the end of the winner's list
-while len(winner_list) != 1:
-
-
-
-
-
-print("Winners: ", winner_list)
-print("Losers: ", loser_list)
-#
-#
-#
-# while winner_col < len(input_list[0]):
-#     for candidate in input_list:
-#         if not winner_list:  # if winner_list is empty (start of check)
-#             if int(candidate[0]) == int(gamma_final[winner_col]):
-#                 winner_list.append(candidate)
-#             else:
-#                 loser_list.append(candidate)
-#         else:
-#             if int(candidate[0]) == int(gamma_final[winner_col]):
-#                 winner_list.append(candidate)
-#             else:
-#                 loser_list.append(candidate)
-#     winner_col += 1
-# #print(winner_list)
-#
-# print("Winners: ", winner_list)
-# print("Losers: ", loser_list)
+# for prev_winner in gamma_final:
+#     winner_list = [element for element in input_list if element.startswith(str(gamma_final[winner_col]))]
+#     loser_list = [element for element in input_list if element.startswith(str(abs(gamma_final[winner_col])-1))]
 
 
 
---
+++ Day04/day04_01.py
@@ -0,0 +1,11 @@
+"""
+Bingo is played on a set of boards each consisting of a 5x5 grid of numbers.
+Numbers are chosen at random, and the chosen number is marked on all boards on which it appears.
+(Numbers may not appear on all boards.) If all numbers in any row or any column of a board are marked, that board wins.
+(Diagonals don't count.)
+
+The submarine has a bingo subsystem to help passengers (currently, you and the giant squid) pass the time.
+It automatically generates a random order in which to draw numbers and a random set of boards (your puzzle input)
+
+"""
+
